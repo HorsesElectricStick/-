70c34a4e47a56af7157d2511486fc24745a1177b
HorsesElectricStick <35952197+HorsesElectricStick@users.noreply.github.com>
2020-04-07 18:13:24 +0800

    Add files via upload

--- autohome/spiders/autohome_spider.py
+++ autohome/spiders/autohome_spider.py
@@ -18,6 +18,12 @@ class AutohomeSpiderSpider(CrawlSpider):
         image_urls2 = response.xpath('//ul[@id="imgList"]//li/a/img/@src2').getall()
         image_urls = image_urls1[:30] + image_urls2
         image_urls = list(map(lambda x:'https:'+x.replace('240x180_0','1024x0_1'),image_urls))
-        title = response.xpath('//ul[@id="imgList"]//li/a/img/@alt').get()
-        item = AutohomeItem(image_urls=image_urls,title=title)
-        return item
+        all_title = response.xpath('//ul[@id="imgList"]//li/a/img/@alt').getall()
+        per_title = []
+        [per_title.append(i) for i in all_title if per_title.count(i)==0]
+        image_dict = {i:[] for i in per_title}
+        for i in range(0,len(image_urls)):
+            image_dict[all_title[i]].append(image_urls[i])
+        for i in per_title:
+            item = AutohomeItem(image_urls=image_dict[i],title=i)
+            yield item
